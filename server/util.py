9cf8bfa18cebc9ff0ccf77ce5c43abcb5512ef38
vrRanjith <71114149+vrRanjith@users.noreply.github.com>
2022-11-09 19:03:04 +0530

    Add files via upload

---
+++ server/util.py
@@ -0,0 +1,46 @@
+import json
+import pickle
+import numpy as np
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_estimated_price(location, sqft, bhk, bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return round(__model.predict([x])[0], 2)
+
+
+def load_saved_artifacts():
+    print("starting to load artifacts...")
+    global __data_columns
+    global __locations
+    global __model
+    with open("./artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+    if __model is None:
+        with open("./artifacts/bangalore_house_price_model.pickle", "rb") as f:
+            __model = pickle.load(f)
+
+    print("loading saved artifacts...Done")
+
+def get_location_names():
+    return __locations
+
+def get_data_columns():
+    return  __data_columns
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
